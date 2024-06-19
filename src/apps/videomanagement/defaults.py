69ca66f6bc04e21187a73bc263cedcf6e4fb9fdb
Konstantinos Damianos <96636678+kostas2370@users.noreply.github.com>
2024-06-19 19:02:52 +0300

    Update defaults.py

--- src/apps/videomanagement/defaults.py
+++ src/apps/videomanagement/defaults.py
@@ -3,5 +3,5 @@ default_format = """
 each section, develop the narration and the description of an image representing the narration
 
 Your answer must be in the following JSON format:
-{"title": -,"audience": -,"genre": -,"scenes": [{"scene",section: [{"narration ","image_description"},]},]}.
+{"title": -,"audience": -,"genre": -,"scenes": [{"scene",sections: [{"narration ","image_description"},]},]}.
 """
